43a5484764d3743b06a73375669aa081f7420809
Pollo47 <82544473+Pollo47@users.noreply.github.com>
2026-05-12 19:10:41 -0400

    Replace RouterProvider with App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router'; // Importamos el router único de router.tsx
+import App from './App'; // Importamos App.tsx
 import { TRPCProvider } from './providers/trpc';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TRPCProvider>
-      <RouterProvider router={router} />
+      <App /> 
     </TRPCProvider>
   </React.StrictMode>
 );
