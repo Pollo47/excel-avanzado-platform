89edebcce71684e73f7c2f89fae5b01ae4ab6b60
Pollo47 <82544473+Pollo47@users.noreply.github.com>
2026-05-12 20:05:06 -0400

    Update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import ExcelCurso from './pages/ExcelCurso';
+import ExcelModule from './pages/ExcelModule'; // <--- IMPORTANTE
+import AdminPage from './pages/AdminPage'; // <--- IMPORTANTE
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Página de Ventas / Inicio */}
+        <Route path="/" element={<Home />} /> 
+        
+        {/* Acceso */}
         <Route path="/login" element={<Login />} />
+        
+        {/* El Tablero del Curso */}
         <Route path="/curso" element={<ExcelCurso />} />
-        <Route path="/" element={<Navigate to="/curso" />} />
-        <Route path="*" element={<Navigate to="/login" />} />
+        
+        {/* La Lección Específica */}
+        <Route path="/excel/module/:moduleId" element={<ExcelModule />} />
+        
+        {/* El Panel de Control de IA Academy */}
+        <Route path="/admin" element={<AdminPage />} />
+        
+        {/* Redirección por defecto */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
